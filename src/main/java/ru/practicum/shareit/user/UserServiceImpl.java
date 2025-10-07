1091ad35159a5deab82acafa0ab1a4c08f59d1de
Aleksei <shokolov@list.ru>
2025-10-08 01:06:13 +0300

    Реализовал основную структуру проекта

--- src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -9,7 +9,6 @@ import ru.practicum.shareit.user.dto.UpdateUserRequest;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.mapper.UserMapper;
 import ru.practicum.shareit.user.model.User;
-
 import java.util.Collection;
 import java.util.Optional;
 import java.util.stream.Collectors;
